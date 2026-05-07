3029c3cb138a2d9602f3682016c9f9bbd60874e0
Bhargavi608 <bhargavitumula2008@gmail.com>
2026-05-07 17:53:10 +0530

    Add files via upload

---
+++ unions.c
@@ -0,0 +1,16 @@
+union car 
+{ char cname [50];
+float price;
+}
+car1,car2;
+printf("enter car1 name & price\n");
+scanf("%s%f",car1.name,&car.price);
+printf("enter car2 name &price\n");
+scanf("%s%f",car2.name,&car.price);
+printf("the details of the car1 are:\n");
+printf("car1 name is %s",car1.name);
+printf("car1 price is %f",car1.price);
+printf("the details of car2 are:\n");
+printf("car2 name is %s",car2.name);
+printf("car2 price is %f",car2.price);
+}
